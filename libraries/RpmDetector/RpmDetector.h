b36536d61dd4af2887d991d10352350873893d26
Joe Dahlquist <joedahlquist@gmail.com>
2014-08-19 10:42:04 -0700

    API work for RpmDetector

--- libraries/RpmDetector/RpmDetector.h
+++ libraries/RpmDetector/RpmDetector.h
@@ -7,14 +7,29 @@
 
 class RpmDetector {
  public:
-  // Pass the interrupt number, not the pin number.
-  RpmDetector(int interrupt);
-  // Get the smoothed, approximate RPM.
+  // interrupt: pass the interrupt number, not the pin number.
+  // starting_rpm: assumed rpm when the wheel first starts rotating.
+  // nominal_rpm: the expected "normal" rpm when the wheel gets up to speed.
+  RpmDetector(int interrupt, int starting_rpm, int nominal_rpm);
+  
+  boolean IsStopped();
+  // Get the current best approximation for the rpm. 
   int Rpm();
+  // Get smoothed rpm.
+  int SmoothedRpm();
+  // Map the smoothed rpm onto the supplied range using the nominal rpm as reference.
+  long MapSmoothedRpm(long min, long max) {
+    return map(SmoothedRpm(), 0, nominal_rpm_, min, max);
+  }
+  
+  int GetNominalRpm();
+  void SetNominalRpm(int rpm);
+  void 
 
  private:
   const int interrupt_;
-  int rpm_;
+  const int starting_rpm_;
+  int nominal_rpm_;
 };
 
 #endif RpmDetector_h

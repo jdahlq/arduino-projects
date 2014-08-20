707328d10edd97154c7d2709a62f25fe7733dbb4
Joe Dahlquist <joedahlquist@gmail.com>
2014-08-19 17:46:34 -0700

    RpmDetector.cc skeleton

--- libraries/RpmDetector/RpmDetector.cpp
+++ libraries/RpmDetector/RpmDetector.cpp
@@ -1,11 +1,26 @@
 #include "Arduino.h"
 #include "RpmDetector.h"
 
-RpmDetector::RpmDetector(int interrupt)
-    : interrupt_(interrupt), rpm_(-1) {
-  // do stuff
+RpmDetector::RpmDetector(int interrupt, int starting_rpm, int nominal_rpm)
+    : interrupt_(interrupt), starting_rpm_(starting_rpm), nominal_rpm_(nominal_rpm) {
+  // TODO: Attach interrupt.
 }
 
 int RpmDetector::Rpm() {
-  return rpm_;
+  // TODO: Implement.
+  return -1;
+}
+
+  
+boolean RpmDetector::IsStopped() {
+  return Rpm() == 0;
+}
+
+int RpmDetector::SmoothedRpm() {
+  // TODO: Implement
+  return -1;
+}
+
+long RpmDetector::MapSmoothedRpm(long min, long max) {
+  return map(SmoothedRpm(), 0, nominal_rpm_, min, max + 1);
 }

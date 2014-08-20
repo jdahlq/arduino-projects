d606491b5b8a6467953e098a08564fb2d6cec7cb
Joe Dahlquist <joedahlquist@gmail.com>
2014-08-19 18:37:26 -0700

    Update RpmDetector.cpp

--- libraries/RpmDetector/RpmDetector.cpp
+++ libraries/RpmDetector/RpmDetector.cpp
@@ -1,19 +1,27 @@
 #include "Arduino.h"
 #include "RpmDetector.h"
 
+namespace {
+  int BlipDeltaToRpm(long blip_delta_ms) {
+    return 60000L / blip_delta_ms;
+  }
+}
+
 RpmDetector::RpmDetector(int interrupt, int starting_rpm, int nominal_rpm)
     : interrupt_(interrupt), starting_rpm_(starting_rpm), nominal_rpm_(nominal_rpm) {
   // TODO: Attach interrupt.
 }
 
 int RpmDetector::Rpm() {
-  // TODO: Implement.
-  return -1;
-}
+  if (IsStopped()) return 0;
+  // If we only have a single blip, use the starting rpm.
+  int last_measured_rpm = last_blip_ == -1 ? starting_rpm_
+                                        : BlipDeltaToRpm(curr_blip_ - last_blip_);
+  int current_rpm_ceiling = BlipDeltaToRpm(millis() - curr_blip_);
 
-  
-boolean RpmDetector::IsStopped() {
-  return Rpm() == 0;
+  // If the last measured rpm is still feasible, use it. Otherwise use our best guess.
+  return last_measured_rpm <= current_rpm_ceiling ? last_measured_rpm
+                                                  : current_rpm_ceiling;
 }
 
 int RpmDetector::SmoothedRpm() {

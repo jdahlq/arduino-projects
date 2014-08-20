2762db3b2595ab0196723bb2a4a565ce98c2d71c
Joe Dahlquist <joedahlquist@gmail.com>
2014-08-19 18:33:51 -0700

    Update RpmDetector.h

--- libraries/RpmDetector/RpmDetector.h
+++ libraries/RpmDetector/RpmDetector.h
@@ -12,9 +12,10 @@ class RpmDetector {
   // nominal_rpm: the expected "normal" rpm when the wheel gets up to speed.
   RpmDetector(int interrupt, int starting_rpm, int nominal_rpm);
   
-  boolean IsStopped();
+  boolean IsStopped() { return stopped_; }
   // Get the current best approximation for the rpm. 
   int Rpm();
+  
   // Get smoothed rpm.
   int SmoothedRpm();
   // Map the smoothed rpm onto the supplied range using the nominal rpm as reference.
@@ -26,9 +27,15 @@ class RpmDetector {
   void SetNominalRpm(int rpm) { nominal_rpm_ = rpm; }
 
  private:
+  void OnFirstBlip();
+  void OnBlip();
+ 
   const int interrupt_;
   const int starting_rpm_;
   int nominal_rpm_;
+  bool stopped_ = true;
+  volatile long curr_blip_ = -1;
+  volatile long last_blip_ = -1;
 };
 
 #endif RpmDetector_h

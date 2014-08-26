fd2e8f0c4b708b660ec24ee0ad40578d7d406204
Joe Dahlquist <joedahlquist@gmail.com>
2014-08-25 18:54:24 -0700

    Remove NeoPixel stuff

--- art_bike/art_bike.ino
+++ art_bike/art_bike.ino
@@ -1,4 +1,3 @@
-#include "Adafruit_NeoPixel.h"
 #include "MusicPlayer.h"
 #include "RpmDetector.h"
 #include "WaveHC.h"
@@ -15,11 +14,6 @@ const int kPlayRateUpdatePeriod = 250;
 long last_play_rate_update = 0;
 long current_play_rate_rpm = 0;
 
-// NeoPixel settings
-const int kNeoPixelPin = 6;
-const long kBeatDetectPeriod = 25;
-long last_beat_detect = 0;
-
 // Create an RpmDetector with a nominal rpm of 120 (~10mph) and a minimum/starting
 // rpm of 10% of nominal.
 RpmDetector rpm_detector(0.1, 120);
@@ -27,31 +21,6 @@ int d = 500;
 float mult = 0.8;
 
 MusicPlayer player;
-
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(90, kNeoPixelPin, NEO_GRB + NEO_KHZ800);
-int magnitude_10 = 0;
-int magnitude_4 = 0;
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  if(WheelPos < 85) {
-    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-  } else if(WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  } else {
-    WheelPos -= 170;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-}
-
-int mod(int a, int b) {
-  int r = a % b;
-  return r >= 0 ? r : r + b;
-}
-
-byte wheel_position;
 
 void setup() {
   Serial.begin(9600);
@@ -63,43 +32,11 @@ void setup() {
   // MusicPlayer setup.
   player.Init();
   player.Play();
-
-  // NeoPixels setup.
-  strip.begin();
-  wheel_position = random(256);
-  byte current_position = wheel_position;
-  for(int i = 0; i < strip.numPixels(); ++i) {
-    current_position += 3;
-    strip.setPixelColor(i, Wheel(current_position));
-  }
-  strip.show();
 }
 
 
 void loop() {
   const long time = millis();
-
-  // Groove the strip to the beat.
-  if (time - last_beat_detect > kBeatDetectPeriod) {
-    last_beat_detect = time;
-    int magnitude = analogRead(A0);
-    magnitude_4 = (magnitude_4 + magnitude) >> 1;
-    magnitude_10 = (magnitude_10 * 15 + magnitude) >> 4;
-    // magnitude_10 = 0.9375 * magnitude_10 + 0.0625 * magnitude;
-    if (magnitude_4 > 1.0625 * magnitude_10) {
-      Serial.print("R7: ");
-      Serial.print(magnitude_10);
-      Serial.print(" ");
-      Serial.println(magnitude_4);
-
-      wheel_position += 3;
-      byte current_position = wheel_position;
-      for(int i = 0; i < strip.numPixels(); ++i) {
-	strip.setPixelColor(i, Wheel(current_position));
-	current_position += 6;
-      }
-    }
-  }
 
   // Adjust playback rate.
   if (time - last_play_rate_update > kPlayRateUpdatePeriod) {
